4c3eaf6b370141dfe3615443e2787ec6195c2d4e
lil climate <lilclimate@gmail.com>
2022-11-10 15:11:30 +0800

    refactor: 优化节点准入判断及插入方式

--- transformer.ts
+++ transformer.ts
@@ -1,4 +1,4 @@
-import { NodeTypes, RootNode } from "./ast";
+import { CallExpressionNode, NodeTypes, RootNode } from "./ast";
 import { traverser } from "./traverser";
 
 export function transformer(ast: RootNode) { 
@@ -11,44 +11,45 @@ export function transformer(ast: RootNode) {
 
 	traverser(ast, {
 		CallExpression: {
-			enter(node, parent) { 
-				if (node.type === NodeTypes.CallExpression) {
-					let expressionNode:any;
+			enter(node, parent) {
+				if (node.type !== NodeTypes.CallExpression) return;
+				let expressionNode:any;
 
+				expressionNode = {
+					type: node.type,	
+					callee: {
+						type: "Identifier",
+						name: node.name,
+					},
+					arguments: [],
+				};
+				node.context = expressionNode.arguments;
+
+				if (parent?.type !== NodeTypes.CallExpression) {
 					expressionNode = {
-						type: node.type,	
-						callee: {
-							type: "Identifier",
-							name: node.name,
-						},
-						arguments: [],
+						type: "ExpressionStatement",
+						expression: expressionNode,
 					};
-					node.context = expressionNode.arguments;
-
-					if (parent?.type !== NodeTypes.CallExpression) {
-						expressionNode = {
-							type: "ExpressionStatement",
-							expression: expressionNode,
-						};
-					}
-
-					parent?.context?.push(expressionNode);
 				}
+
+				pushNode(parent, expressionNode);
 			}
 		},
 		NumberLiteral: {
-			enter(node, parent) { 
-				if (node.type === NodeTypes.NumberLiteral) {
-					const numberNode = {
-						type: node.type,
-						value: node.value,
-					};
-					parent?.context?.push(numberNode);
-				}
+			enter(node, parent) {
+				if (node.type !== NodeTypes.NumberLiteral) return;
+				const numberNode = {
+					type: node.type,
+					value: node.value,
+				};
+				pushNode(parent, numberNode);
 			}
 		},
 	});
 
-	console.log('newAst: ', JSON.stringify(newAst));
 	return newAst;
+}
+
+function pushNode(parent: RootNode|CallExpressionNode|undefined, data: any) {
+	parent?.context?.push(data);
 }
